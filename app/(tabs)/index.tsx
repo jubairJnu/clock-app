92258c7652a60ee523a881a683bab90705fc479d
jubairJnu <jubair2810@gmail.com>
2024-08-23 23:29:48 +0600

    clock-app initialize

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,247 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_700Bold,
+} from "@expo-google-fonts/inter";
+import { useState } from "react";
+import {
+  Image,
+  StyleSheet,
+  Platform,
+  View,
+  Text,
+  ActivityIndicator,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+// expanded
+
+type TRowProps = {
+  label: string;
+  value: string;
+};
+
+const RowView = ({ label, value }:TRowProps) => {
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: 14,
+      }}
+    >
+      <View>
+        <Text
+          style={{
+            fontFamily: "Inter-Regular",
+            fontSize: 10,
+            color: "#303030",
+            letterSpacing: 2,
+            textTransform: "uppercase",
+          }}
+        >
+          {label}
+        </Text>
+      </View>
+      <View>
+        <Text
+          style={{
+            fontFamily: "Inter-bold",
+            fontSize: 20,
+            color: "#303030",
+
+            textTransform: "capitalize",
+          }}
+        >
+          {value}
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+// main
 
 export default function HomeScreen() {
+  const [showMore, setShowMore] = useState(false);
+
+  let [fontsLoaded] = useFonts({
+    "Inter-Regular": Inter_400Regular,
+    "Inter-bold": Inter_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator />;
+  }
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <ImageBackground
+      source={require("../../assets/light-bg.png")}
+      resizeMode="cover"
+      style={{ flex: 1 }}
+    >
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "space-between",
+          marginTop: 65,
+          paddingHorizontal: 28,
+        }}
+      >
+        {/* upper */}
+
+        {!showMore && (
+          <View style={{ flexDirection: "row" }}>
+            <View style={{ flex: 1 }}>
+              <Text
+                style={{
+                  fontFamily: "Inter-Regular",
+                  fontSize: 12,
+                  color: "#fff",
+                }}
+              >
+                “The science of operations, as derived from mathematics more
+                especially, is a science of itself, and has its own abstract
+                truth and value.”
+              </Text>
+
+              <Text
+                style={{
+                  marginTop: 8,
+                  fontFamily: "Inter-bold",
+                  color: "#fff",
+                }}
+              >
+                - Ada Lovelace
+              </Text>
+            </View>
+            {/* icon */}
+            <Image source={require("../../assets/refresh.png")} />
+          </View>
+        )}
+
+        {/* upper closse */}
+
+        {/* bottom portion */}
+
+        <View style={{ marginBottom: 26 }}>
+          {/* icon and text */}
+          <View
+            style={{
+              flexDirection: "row",
+            }}
+          >
+            <Image source={require("../../assets/sun.png")} />
+            <Text
+              style={{
+                marginLeft: 8,
+                fontFamily: "Inter-Regular",
+                fontSize: 15,
+                color: "#fff",
+                letterSpacing: 3,
+                textTransform: "uppercase",
+              }}
+            >
+              Good Morning
+            </Text>
+          </View>
+          {/* time */}
+
+          <View>
+            <Text>
+              <Text
+                style={{
+                  fontFamily: "Inter-bold",
+                  fontSize: 100,
+                  color: "#fff",
+                }}
+              >
+                11:37
+              </Text>
+              <Text
+                style={{
+                  fontFamily: "Inter-Regular",
+                  fontSize: 15,
+                  color: "#fff",
+                }}
+              >
+                BST
+              </Text>
+            </Text>
+          </View>
+          {/* location */}
+
+          <View>
+            <Text
+              style={{
+                textTransform: "uppercase",
+                fontFamily: "Inter-bold",
+                fontSize: 15,
+                color: "white",
+                letterSpacing: 3,
+              }}
+            >
+              in london uk
+            </Text>
+          </View>
+
+          {/* button*/}
+
+          <TouchableOpacity
+            onPress={() => {
+              setShowMore(!showMore);
+            }}
+            style={{
+              flexDirection: "row",
+              height: 40,
+              width: 115,
+              backgroundColor: "#fff",
+              borderRadius: 30,
+              justifyContent: "space-between",
+              alignItems: "center",
+              paddingLeft: 16,
+              paddingRight: 4,
+              marginTop: 50,
+            }}
+          >
+            <Text
+              style={{
+                fontFamily: "Inter-bold",
+                letterSpacing: 3,
+                textTransform: "uppercase",
+              }}
+            >
+              {showMore ? "Less" : "More"}
+            </Text>
+            <Image
+              source={
+                showMore
+                  ? require("../../assets/arrow-up.png")
+                  : require("../../assets/arrow-down.png")
+              }
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      {/* expanded view */}
+
+      {showMore && (
+        <View
+          style={{
+            backgroundColor: "#fff",
+            opacity: 0.8,
+            paddingVertical: 48,
+            paddingHorizontal: 26,
+          }}
+        >
+          <RowView label={"Current Timezone"} value={"europe/london"} />
+          <RowView label={"Day of the Year"} value={"295"} />
+          <RowView label={"day of the week"} value={"5"} />
+          <RowView label={"week  number"} value={"42"} />
+        </View>
+      )}
+    </ImageBackground>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
